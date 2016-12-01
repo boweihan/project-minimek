2914ddee19b67f7e435d35c6443424e44b64927c
Mark Erikson <mark@isquaredsoftware.com>
2016-11-30 21:13:36 -0500

    Add initial unit info reducer and connection

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
 import tabReducer from "features/tabs/tabReducer";
+import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 
 const rootReducer = combineReducers({
+<<<<<<< HEAD:src/reducers/rootReducer.js
   tabs: tabReducer
+=======
+    unitInfo : unitInfoReducer,
+    tabs : tabReducer,
+>>>>>>> bce3a2d... Add initial unit info reducer and connection:src/app/reducers/rootReducer.js
 });
 
 export default rootReducer;
