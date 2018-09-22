622968f0d9105ad38c9d6925badb40db26804c6e
bowei <bowei.han100@gmail.com>
2018-09-22 17:45:33 -0400

    Project setup with HMR

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+import configureStore from "./store/configureStore";
+const store = configureStore();
+
+// save a ref to the root element
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  const App = require("./App").default;
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  // Support hot reloading of components
+  // and display an overlay for runtime errors
+  const renderApp = render;
+  const renderError = error => {
+    const RedBox = require("redbox-react").default;
+    ReactDOM.render(<RedBox error={error} />, rootEl);
+  };
+	
+	render = () => {
+		try {
+			renderApp();
+		}
+		catch(error) {
+			console.error(error);
+			renderError(error);
+		}
+	}
+
+	module.hot.accept("./App", () => {
+		setTimeout(render);
+	});
+}
+
+render();
 registerServiceWorker();
