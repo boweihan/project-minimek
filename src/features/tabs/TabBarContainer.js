7d4ddb31c6fd80b13f7eb89670ce615495941cce
bowei <bowei.han100@gmail.com>
2018-09-22 20:24:51 -0400

    base tabs

--- src/features/tabs/TabBarContainer.js
+++ src/features/tabs/TabBarContainer.js
@@ -1,31 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import TabBar from "./TabBar";
 
-export default class TabBarContainer extends Component {
-  constructor(props) {
-    super(props);
-    const { tabs = [{ name: null }] } = props;
-    const firstTab = tabs[0];
-    this.state = {
-      currentTab: firstTab.name
-    };
-  }
+import { selectCurrentTab } from "./tabSelectors";
+import { selectTab } from "./tabActions";
 
-  onTabClick = name => {
-    this.setState({ currentTab: name });
-  };
+const mapState = state => {
+  const currentTab = selectCurrentTab(state);
+  return { currentTab };
+};
 
-  render() {
-    const { tabs, ...otherProps } = this.props;
-    const { currentTab } = this.state;
-    return (
-      <TabBar
-        {...otherProps}
-        currentTab={currentTab}
-        onTabClick={this.onTabClick}
-        tabs={tabs}
-      />
-    );
-  }
-}
+const actions = { onTabClick: selectTab };
+
+export default connect(
+  mapState,
+  actions
+)(TabBar);
