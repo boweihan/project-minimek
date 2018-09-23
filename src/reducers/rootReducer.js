d6e7312c80b5347879c7ce0b90333739fed6ceb3
bowei <bowei.han100@gmail.com>
2018-09-23 09:42:57 -0400

    unit details reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,12 +3,8 @@ import tabReducer from "features/tabs/tabReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 
 const rootReducer = combineReducers({
-<<<<<<< HEAD:src/reducers/rootReducer.js
-  tabs: tabReducer
-=======
     unitInfo : unitInfoReducer,
     tabs : tabReducer,
->>>>>>> bce3a2d... Add initial unit info reducer and connection:src/app/reducers/rootReducer.js
 });
 
 export default rootReducer;
